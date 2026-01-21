fc3a5c2681c6fca178dfb24f2c73437aceb03cb2
Mattias Svensson <mattsve@users.noreply.github.com>
2026-01-21 13:25:23 +0100

    Update lxd-export.sh
    fix spelling error in lxd-export.sh

--- ansible/playbooks/files/lxd/lxd-export.sh
+++ ansible/playbooks/files/lxd/lxd-export.sh
@@ -2,5 +2,5 @@
 for i in $(lxc list -c n --format csv)
 do
      echo "Making backup of ${i} ..."
-     lxc export "${i}" "/backups/lxd/${i}-backup.tar.xz" --optimized-storage
+     lxc export "${i}" "/backup/lxd/${i}-backup.tar.xz" --optimized-storage
 done
